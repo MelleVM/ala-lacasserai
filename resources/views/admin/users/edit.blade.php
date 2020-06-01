4addf055ac0c93ed32cfa6344d7ff612f0fe86b8
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-06-01 20:06:28 +0200

    Update edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,47 +1,66 @@
-{{-- admin/users/edit.blade.php --}}
+{{-- admin/bookings/edit.blade.php --}}
 
 {{-- gebruikt 'admin' layout --}}
 @extends('layouts.admin')
 
 {{-- content sectie --}}
 @section('content')
+
 <div class="container-admin">
     <div class="table-wrapper">
         <div class="table-title">
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Edit <b>User</b></h2>
+                    <h2>Edit <b>Booking</b></h2>
                 </div>
             </div>
         </div>
 
-        {!! Form::model($user, ['method' => 'PUT', 'route' => ['admin.users.update', $user->id]]) !!}
-
+        {!! Form::model($booking, ['method' => 'PUT', 'route' => ['admin.bookings.update', $booking->id]]) !!}
+        <div class="form-group">
+            <label>Time From</label>
+            <input value="{{$booking->time_from}}" autocomplete="off" type="text" name="time_from" class="form-control datetimepicker" placeholder="Select date">
+        </div>
+        <div class="form-group">
+            <label>Time To</label>
+            <input value="{{$booking->time_to}}" autocomplete="off" type="text" name="time_to" class="form-control datetimepicker" placeholder="Select date">
+        </div>
+        <div class="form-group">
+            <label>Price</label>
+            <input value="{{$booking->price}}" type="text" name="price" class="form-control" placeholder="Enter price">
+        </div>
+        <div class="form-group">
+            <label>Room Number</label>
+            <input value="{{$booking->room_number}}" type="text" name="room_number" class="form-control" placeholder="Enter room number">
+        </div>
+        <div class="form-group">
+            {{Form::label('payed', 'Payed')}}
+            {{Form::select('payed', [true => 'True', false => 'False'], $booking->payed, ['class' => 'browser-default custom-select'])}}
+        </div>
             <div class="form-group">
-                <label for="exampleInputName1">Name</label>
-                <input type="text" value="@isset($user->name){{$user->name}}@endisset" name="name" class="form-control" id="exampleInputName1" aria-describedby="emailHelp"
-                    placeholder="Enter name">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" value="@isset($user->email){{$user->email}}@endisset" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                    else.</small>
-            </div>
-
-            <div class="form-group">
-
-            {{Form::label('role_id', 'Role')}}
-            {{Form::select('role_id', ['1' => 'User', '2' => 'Moderator', '3' => 'Administrator'], $user->role_id, ['class' => 'browser-default custom-select', 'id' => 'exampleInputRole1'])}}
-
-            </div>
-            {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
-            {!! Form::close() !!}﻿
+            {{Form::label('state', 'State')}}
+            {{Form::select('state', ['confirmed' => 'Confirmed', 'pending' => 'Pending'], $booking->state, ['class' => 'browser-default custom-select'])}}
+        </div>
+        <div class="form-group">
+            <label>Payment Method</label>
+            <input value="{{$booking->payment_method}}" type="text" name="payment_method" class="form-control" placeholder="Enter payment method">
+        </div>
+        <div class="form-group">
+            {!! Form::label('room_id', trans('Room ID').'', ['class' => 'control-label']) !!}
+            {!! Form::select('room_id', $rooms, old('room_id'), ['class' => 'form-control select2']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('user_id', trans('User ID').'', ['class' => 'control-label']) !!}
+            {!! Form::select('user_id', $users, old('user_id'), ['class' => 'form-control select2']) !!}
+        </div>
+        <div class="form-group">
+            <label>Additional Information</label>
+            <textarea placeholder="Additional Information" name="additional_information" class="w-100 p-2"
+                rows="8">{{$booking->additional_information}}</textarea>
+        </div>
+        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+        {!! Form::close() !!}﻿
     </div>
 </div>
-
-
-   
-@endsection
+@stop
 
