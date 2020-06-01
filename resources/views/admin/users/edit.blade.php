07139517852cf3e5c2b9389add1f8c44b5639a4b
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-06-01 20:24:32 +0200

    Update edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,66 +1,81 @@
-{{-- admin/bookings/edit.blade.php --}}
+{{-- admin/users/edit.blade.php --}}
 
 {{-- gebruikt 'admin' layout --}}
 @extends('layouts.admin')
 
 {{-- content sectie --}}
 @section('content')
-
 <div class="container-admin">
     <div class="table-wrapper">
         <div class="table-title">
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Edit <b>Booking</b></h2>
+                    <h2>Edit <b>User</b></h2>
                 </div>
             </div>
         </div>
 
-        {!! Form::model($booking, ['method' => 'PUT', 'route' => ['admin.bookings.update', $booking->id]]) !!}
-        <div class="form-group">
-            <label>Time From</label>
-            <input value="{{$booking->time_from}}" autocomplete="off" type="text" name="time_from" class="form-control datetimepicker" placeholder="Select date">
-        </div>
-        <div class="form-group">
-            <label>Time To</label>
-            <input value="{{$booking->time_to}}" autocomplete="off" type="text" name="time_to" class="form-control datetimepicker" placeholder="Select date">
-        </div>
-        <div class="form-group">
-            <label>Price</label>
-            <input value="{{$booking->price}}" type="text" name="price" class="form-control" placeholder="Enter price">
-        </div>
-        <div class="form-group">
-            <label>Room Number</label>
-            <input value="{{$booking->room_number}}" type="text" name="room_number" class="form-control" placeholder="Enter room number">
-        </div>
-        <div class="form-group">
-            {{Form::label('payed', 'Payed')}}
-            {{Form::select('payed', [true => 'True', false => 'False'], $booking->payed, ['class' => 'browser-default custom-select'])}}
-        </div>
+        {!! Form::model($user, ['method' => 'PUT', 'route' => ['admin.users.update', $user->id]]) !!}
+
             <div class="form-group">
-            {{Form::label('state', 'State')}}
-            {{Form::select('state', ['confirmed' => 'Confirmed', 'pending' => 'Pending'], $booking->state, ['class' => 'browser-default custom-select'])}}
-        </div>
-        <div class="form-group">
-            <label>Payment Method</label>
-            <input value="{{$booking->payment_method}}" type="text" name="payment_method" class="form-control" placeholder="Enter payment method">
-        </div>
-        <div class="form-group">
-            {!! Form::label('room_id', trans('Room ID').'', ['class' => 'control-label']) !!}
-            {!! Form::select('room_id', $rooms, old('room_id'), ['class' => 'form-control select2']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('user_id', trans('User ID').'', ['class' => 'control-label']) !!}
-            {!! Form::select('user_id', $users, old('user_id'), ['class' => 'form-control select2']) !!}
-        </div>
-        <div class="form-group">
-            <label>Additional Information</label>
-            <textarea placeholder="Additional Information" name="additional_information" class="w-100 p-2"
-                rows="8">{{$booking->additional_information}}</textarea>
-        </div>
-        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
-        {!! Form::close() !!}﻿
+                <label>Name</label>
+                <input type="text" value="@isset($user->name){{$user->name}}@endisset" name="name" class="form-control"
+                    placeholder="Enter name">
+            </div>
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" value="@isset($user->email){{$user->email}}@endisset" name="email" class="form-control"
+                    placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                    else.</small>
+            </div>
+            <div class="form-group">
+                <label>Address</label>
+                <input type="text" value="@isset($user->address){{$user->address}}@endisset" name="address" class="form-control"
+                    placeholder="Enter address">
+            </div>
+            <div class="form-group">
+                <label>Residence</label>
+                <input type="text" value="@isset($user->residence){{$user->residence}}@endisset" name="residence" class="form-control"
+                    placeholder="Enter residence">
+            </div>
+            <div class="form-group">
+                <label>Postal code</label>
+                <input type="text" value="@isset($user->postal_code){{$user->postal_code}}@endisset" name="postal_code" class="form-control"
+                    placeholder="Enter postal code">
+            </div>
+            <div class="form-group">
+                <label>Checked in until</label>
+                <input type="text" value="@isset($user->checked_in_to){{$user->checked_in_to}}@endisset" name="checked_in_to" class="form-control datetimepicker"
+                    placeholder="Enter checked in until">
+            </div>
+            <div class="form-group">
+                <label>Checked in room</label>
+                <input type="text" value="@isset($user->checked_in_room){{$user->checked_in_room}}@endisset" name="checked_in_room" class="form-control"
+                    placeholder="Enter checked in room">
+            </div>
+            <div class="form-group">
+                <label>Checked out room</label>
+                <input type="text" value="@isset($user->checked_out_room){{$user->checked_out_room}}@endisset" name="checked_out_room" class="form-control"
+                    placeholder="Enter checked out room">
+            </div>
+            <div class="form-group">
+                <label>Checked out time</label>
+                <input type="text" value="@isset($user->checked_out_time){{$user->checked_out_time}}@endisset" name="checked_out_room" class="form-control datetimepicker"
+                    placeholder="Enter checked out time">
+            </div>
+            <div class="form-group">
+
+            {{Form::label('role_id', 'Role')}}
+            {{Form::select('role_id', ['1' => 'User', '2' => 'Moderator', '3' => 'Administrator'], $user->role_id, ['class' => 'browser-default custom-select', 'id' => 'exampleInputRole1'])}}
+
+            </div>
+            {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+            {!! Form::close() !!}﻿
     </div>
 </div>
-@stop
+
+
+   
+@endsection
 
