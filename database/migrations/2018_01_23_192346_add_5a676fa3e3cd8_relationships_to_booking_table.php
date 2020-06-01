67f2cff4973a8378a55582c1a07b9753883d42fb
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-06-01 15:29:56 +0200

    Update 2018_01_23_192346_add_5a676fa3e3cd8_relationships_to_booking_table.php

--- database/migrations/2018_01_23_192346_add_5a676fa3e3cd8_relationships_to_booking_table.php
+++ database/migrations/2018_01_23_192346_add_5a676fa3e3cd8_relationships_to_booking_table.php
@@ -15,15 +15,11 @@ class Add5a676fa3e3cd8RelationshipsToBookingTable extends Migration
         Schema::table('bookings', function(Blueprint $table) {
             if (!Schema::hasColumn('bookings', 'user_id')) {
                 $table->integer('user_id')->unsigned()->nullable();
-                $table->foreign('user_id', '110461_5a676fa2321c7')->references('id')->on('users')->onDelete('cascade');
+                $table->foreign('user_id', '110461_5a676fa2321c7')->references('id')->on('users');
                 }
                 if (!Schema::hasColumn('bookings', 'room_id')) {
                 $table->integer('room_id')->unsigned()->nullable();
-                $table->foreign('room_id', '110461_5a676fa239ffd')->references('id')->on('rooms')->onDelete('cascade');
-                }
-                if (!Schema::hasColumn('bookings', 'room_number')) {
-                $table->integer('room_number')->unsigned()->nullable();
-                $table->foreign('room_number', '110461_5a676fa239ffd')->references('room_number')->on('rooms')->onDelete('cascade');
+                $table->foreign('room_id', '110461_5a676fa239ffd')->references('id')->on('rooms');
                 }
         });
     }
