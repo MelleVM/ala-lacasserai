0bace1768fc78afee4d1b9b887d81237ab699ee9
MelleVM <45255788+MelleVM@users.noreply.github.com>
2020-06-01 15:18:09 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $items = [
+            
+            ['id' => 1, 'name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '$2y$10$akHCvTRpvma2eB8VOqUEoOtpWEelS2/e2TZK3LJyfLxuvw8MrQxVq', 'role_id' => 3, 'remember_token' => '', 'address' => '', 'residence' => '', 'postal_code' => ''],
+
+        ];
+
+        foreach ($items as $item) {
+            \App\User::create($item);
+        }
     }
 }
